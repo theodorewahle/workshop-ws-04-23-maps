b9ae7e65ab714d083036edd0d3177914ba4ede25
Theodore Wahle <theodorewahle@gmail.com>
2019-04-23 15:14:26 -0400

    Completed workshop

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.881504923f36837713522351f28b7458.js"
+  "/precache-manifest.15d04b6ea7ab6f9b086a036ba18b2c6c.js"
 );
 
 workbox.clientsClaim();
